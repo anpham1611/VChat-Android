4f869c9b3193e239d04b8c1ed8f95bf3c526fe2f
Anthony Pham <an.pham1611@gmail.com>
2016-02-25 08:56:13 +0700

    Save

--- app/src/main/java/com/dounets/vchat/ui/uicontroller/MainActivityUiController.java
+++ app/src/main/java/com/dounets/vchat/ui/uicontroller/MainActivityUiController.java
@@ -2,10 +2,13 @@ package com.dounets.vchat.ui.uicontroller;
 
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.Button;
 
 import com.dounets.vchat.R;
 import com.dounets.vchat.ui.activity.MainActivity;
+import com.etsy.android.grid.StaggeredGridView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -13,14 +16,14 @@ import butterknife.ButterKnife;
 /**
  * Created by an.pham1611 on 2/23/16.
  */
-public class MainActivityUiController implements View.OnClickListener {
+public class MainActivityUiController implements View.OnClickListener, AbsListView.OnScrollListener, AbsListView.OnItemClickListener, AdapterView.OnItemLongClickListener {
     MainActivity activity;
 
     @Bind(R.id.toolbar)
     Toolbar toolBar;
 
-    @Bind(R.id.button)
-    Button recordButton;
+    @Bind(R.id.grid_view)
+    StaggeredGridView gridView;
 
     public MainActivityUiController(MainActivity activity) {
         this.activity = activity;
@@ -30,7 +33,6 @@ public class MainActivityUiController implements View.OnClickListener {
 
     private void init() {
         initToolbar();
-        recordButton.setOnClickListener(this);
     }
 
     private void initToolbar() {
@@ -41,13 +43,30 @@ public class MainActivityUiController implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.button: {
-                activity.onClickRecord();
-            }
-            break;
+
             default:
                 activity.onBackPressed();
                 break;
         }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        return false;
+    }
+
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+    }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
     }
 }
